42fa166cf306b022d4124ab974aaa83da9900142
KKG <lancexk@gmail.com>
2018-06-29 14:02:53 +0800

    non-algorithm part should be uploaded

---
+++ StockModal/DBLoader.py
@@ -0,0 +1,52 @@
+import os, apsw
+#from PyQt4 import QtCore
+import websocket
+
+DB_Path='E:/SA/StockAssist_0304/DB/final.db'
+
+def loadDB(sid):
+    db = DBLoad(sid)
+    db.setFileName()
+    db.run()
+    return db.memconn
+
+class DBLoad:
+    def __init__(self,sid, parent=None):
+        self.cancel = False
+        self.sid=sid
+    
+    def setFileName(self, fileName=DB_Path):
+        self.fileName = fileName
+        #self.progressText.emit("Loading %s (%%p%%)" % fileName, True)
+        
+    def run(self):
+        self.phyconn = apsw.Connection(self.fileName)
+        self.memconn = apsw.Connection(":memory:")
+        try:#backup.__exit__() just make sure copy if finished,not close backup,so with is good,memconn is still exist when out
+            with self.memconn.backup("main", self.phyconn, "main") as backup:
+                # call with 0 to get the total pages
+                backup.step(0)
+                total = backup.pagecount
+                
+                stepped = 0
+                one_percent = total if total < 100 else total // 100
+                last_percentage = 0
+                while stepped <= total:
+                    if self.cancel:
+                        #self.progressCanceled.emit()
+                        self.memconn=None
+                        return
+                    backup.step(one_percent)
+                    stepped = stepped + one_percent
+                    stepped_percentage = stepped*100//total
+                    if stepped_percentage != last_percentage:
+                        last_percentage = stepped_percentage
+                        #self.progressChanged.emit(stepped_percentage)
+                        websocket.UpdateLoadDBProgress(stepped_percentage,self.sid)
+                # Done
+                #self.progressFinished.emit()
+        except :
+            #self.progressError.emit("Error when loading database!")
+            self.memconn=None
+            pass
+            
---
+++ StockModal/SinaHTMLParser.py
@@ -0,0 +1,123 @@
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+from struct import pack
+from collections import namedtuple
+import threading
+import websocket
+import GSym
+TTagState = namedtuple("TTagState", "tag attr value next data")
+
+class Matched(Exception): pass
+
+class SinaHTMLParser(HTMLParser):
+    def __init__(self, sid,sina_dir,parent=None):#progress_que,
+        #self.que=progress_que
+        self.sid=sid
+        self.endTagStates = ("table", None, None, None, False)
+        self.setParserFromat(True)
+        self.stockName = None
+        self.file = None
+        self.sina_dir_path=sina_dir
+        self.fileFormat = ("date", "shares", "value", "factor", "open", "high", "close", "low")
+        
+        super(SinaHTMLParser, self).__init__(parent)
+        
+    def newStock(self):
+        if self.file is not None:
+            self.file.close()
+        self.stockName = None
+        self.file = None
+        
+    def setParserFromat(self, new):
+        if new:
+            self.startTagStates = {"begin" : ("table", "id", "FundHoldSharesTable", "name", False),
+                                   "name"  : ("th", "colspan", "8", "date", True),
+                                   "date"  : ("a", "target", "_blank", "open", True),
+                                   "open"  : ("div", None, None, "high", True),
+                                   "high"  : ("div", None, None, "close", True),
+                                   "close" : ("div", None, None, "low", True),
+                                   "low"   : ("div", None, None, "shares", True),
+                                   "shares": ("div", None, None, "value", True),
+                                   "value" : ("div", None, None, "factor", True),
+                                   "factor": ("div", None, None, "date", True)}
+        else:
+            self.startTagStates = {"begin" : ("table", "id", "FundHoldSharesTable", "name", False),
+                                   "name"  : ("th", "colspan", "8", "tr1", True),
+                                   "tr1"   : ("tr", None, None, "tr2", False),
+                                   "tr2"   : ("tr", None, None, "date", False),
+                                   "date"  : ("div", None, None, "open", True),
+                                   "open"  : ("div", None, None, "high", True),
+                                   "high"  : ("div", None, None, "close", True),
+                                   "close" : ("div", None, None, "low", True),
+                                   "low"   : ("div", None, None, "shares", True),
+                                   "shares": ("div", None, None, "value", True),
+                                   "value" : ("div", None, None, "factor", True),
+                                   "factor": ("div", None, None, "date", True)}
+        self.startTagKey = "begin"
+        self.startTag = TTagState._make(self.startTagStates[self.startTagKey])
+        self.endTag = None
+        self.dataAvial = False
+        self.hasRealData = False
+        self.dataEnd = False
+        self.row = {}
+        
+    def decode(self, tag, attrs):
+        if tag == self.startTag.tag:
+            try:
+                if self.startTag.attr is None:                    
+                    raise Matched()
+                for attr in attrs:
+                    if attr[0] == self.startTag.attr and attr[1] == self.startTag.value:
+                        raise Matched()
+            except Matched:
+                if self.endTag is None:
+                    self.endTag = TTagState._make(self.endTagStates)
+                self.dataAvial = self.startTag.data
+                # only update when data is not cared
+                if not self.dataAvial:
+                    self.startTagKey = self.startTag.next
+                    self.startTag = TTagState._make(self.startTagStates[self.startTagKey])
+                    
+    def processData(self):
+        self.row["date"] = (lambda x, y, z: x*10000+y*100+z)(*list(map(int, self.row["date"].split("-"))))
+        self.row["shares"] = float(self.row["shares"])
+        self.row["value"] = float(self.row["value"])
+        for field in self.fileFormat[3:]:
+            self.row[field] = int(float(self.row[field])*1000)
+    
+    def flushData(self):
+        if self.file is None:
+            self.file = open(self.sina_dir_path+"//"+self.stockName+".sina", "wb")
+            #self.file = open("d:/sina/"+self.stockName+".sina", "wb")
+        self.file.write(pack("<lfflllll", *list(map(self.row.get, self.fileFormat))))    
+        
+    def handle_starttag(self, tag, attrs):
+        self.decode(tag, attrs)
+        
+    def handle_endtag(self, tag):
+        if self.endTag is not None and self.endTag.tag == tag:
+            self.dataEnd = True
+            
+    def handle_data(self, data):
+        if not self.dataEnd and self.dataAvial:
+            if self.startTagKey == "name":
+                if self.stockName is None: 
+                    self.stockName = data.rstrip(" \r\t\n").lstrip(" \r\t\n").replace("*", "_")
+                    if self.stockName != "()": # means this stock has no FuQuan data
+                        websocket.UpdateSpideredName(self.stockName,self.sid)
+                        #self.que.put(self.stockName)
+                        print(self.stockName)
+                        self.noName = False
+                    else:
+                        self.noName = True
+            else:
+                self.hasRealData = True
+                self.row[self.startTagKey] = data.rstrip(" \r\t\n").lstrip(" \r\t\n")
+            self.dataAvial = False
+            self.startTagKey = self.startTag.next
+            self.startTag = TTagState._make(self.startTagStates[self.startTagKey])
+            # all data fields are collected
+            if self.startTagKey == "date" and self.hasRealData:
+                self.processData()
+                self.flushData()
+                    
